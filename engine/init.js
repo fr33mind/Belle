4162e0e7fc89ab3293b8d6660a4739c8c66549ed
freemind <freemind@live.com.pt>
2015-01-25 04:12:00 +0000

    Add some new functions and properties to belle namespace.

--- engine/init.js
+++ engine/init.js
@@ -1,42 +1,119 @@
 belle = {};
 belle.actions = {};
 belle.objects = {};
-belle.display = {};
 belle.utils = {};
-belle.stateMachine = {};
-
-game = {
-    "resources" : {},
-    "customFonts" : [],
-    "scenes" : [],
-    "scene": null,
-    "ready" : false,
-    "width": 640,
-    "height" : 480,
-    "currentWidth": 640,
-    "currentHeight": 480,
-    "variables" : {},
-    "finished" : false,
-    "textSpeed" : 10,
-    "textDelay" : 100,
-    "filename" : "game.json",
-    "directory" : "",
-    "states" : {},
-    "data" : null,
-    "paused": false,
-    "eventListeners" : {
-      "variableChanged" : []
-    },
-    "soundManager": {}
+belle.core = {};
+belle.views = {};
+belle.display = {};
+belle.graphics = {};
+belle.game = {
+  directory: ""
 };
+function surrogateCtor() {}
 
-game.pauseScreen = {
-    "scenes" : [],
-    "scene": null,
-    "states" : {},
-    "finished": false
-};
+belle.extend = function (sub, base)
+{
+    // Copy the prototype from the base to setup inheritance
+    surrogateCtor.prototype = base.prototype;
+    // Tricky huh?
+    sub.prototype = new surrogateCtor();
+    // Remember the constructor property was set wrong, let's fix it
+    sub.prototype.constructor = sub;
+}
 
+belle.isInstance = function(object, constructor) {
+
+  while (object != null) {
+    if (object == constructor.prototype)
+      return true;
+    object = Object.getPrototypeOf(object);
+  }
+  return false;
+}
+
+belle.serialize = function(obj)
+{
+  var prop;
+
+  if (! obj)
+    return null;
+
+  if (typeof obj == "number" || typeof obj == "string" || typeof value == "boolean")
+    return obj;
+  else if (obj instanceof Array) {
+    prop = [];
+    for(var i=0; i < obj.length; i++)
+      prop[i] = belle.serialize(obj[i]);
+  }
+  else if (typeof obj == "object") {
+    prop = {};
+    //if derived from Belle's Object, use its serialize function
+    if (typeof obj.serialize == "function"){
+      prop = obj.serialize();
+    }
+    else {
+      for(key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          prop[key] = belle.serialize(obj[key]);
+        }
+      }
+    }
+  }
+
+  return prop;
+}
+
+belle.createObject = function(data, parent)
+{
+    var type = data["type"];
+    if (! type) {
+        belle.log("Object has no type");
+        return null;
+    }
+
+    var ObjectClass = belle.objects[type];
+
+    if (! ObjectClass) {
+        belle.log("'" + type + "' is not a valid object type.");
+        return null;
+    }
+
+
+    var obj = new ObjectClass(data, parent);
+
+    return obj;
+}
+
+belle.createAction = function(data, parent)
+{
+    var type = data["type"];
+    var _Action = belle.actions[type];
+
+    if (! _Action) {
+        belle.log("'" + type + "' is not a valid action type.");
+        return null;
+    }
+
+    return new _Action(data, parent);
+}
+
+belle.isNumber = function(num)
+{
+  num = parseInt(num);
+  return (num === num);
+}
+
+//set requestAnimationFrame
+window.requestAnimationFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.oRequestAnimationFrame      ||
+          window.msRequestAnimationFrame     ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
 
 //Extend Array and String objects
 if (typeof String.prototype.startsWith != 'function') 
