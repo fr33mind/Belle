9db50dbaedf909457c446dd3da6a1586b483759a
fr33mind <freemind@live.com.pt>
2018-01-29 00:10:12 +0000

    Allow negative numbers in NumberValidator.

--- editor/widgets/numbervalidator.cpp
+++ editor/widgets/numbervalidator.cpp
@@ -1,6 +1,6 @@
 #include "numbervalidator.h"
 
-static const QRegularExpression mStaticRegExp("^\\d+\\.?\\d*$");
+static const QRegularExpression mStaticRegExp("^\-?\\d+\\.?\\d*$");
 
 NumberValidator::NumberValidator(QObject *parent) :
     QValidator(parent)
@@ -13,9 +13,13 @@ QValidator::State NumberValidator::validate(QString & string, int & pos) const
     if (string.isEmpty())
         return Intermediate;
 
-    QRegularExpressionMatch match = mRegExp.match(string);
+    QRegularExpressionMatch match = mRegExp.match(string, 0, QRegularExpression::PartialPreferCompleteMatch);
+
     if (match.hasMatch())
         return Acceptable;
+    else if (match.hasPartialMatch())
+        return Intermediate;
+
     return Invalid;
 }
 
